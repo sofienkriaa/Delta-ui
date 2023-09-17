b0f7bbc5180f9b9d6c1c88ccfcb55217bed2c310
Sofien Kriaa <kriasofien@gmail.com>
2023-09-17 15:39:46 +0200

    Add view control

---
+++ Assets/rotateCamera.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCamera : MonoBehaviour
+{
+    public float sensitivity = 1;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /*void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed left-click.");
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+            Debug.Log("Pressed right-click.");
+            float rotateHorizontal = Input.GetAxis("Mouse X");
+            float rotateVertical = Input.GetAxis("Mouse Y");
+            transform.Rotate(-transform.up * rotateHorizontal * sensitivity);
+            transform.Rotate(transform.right * rotateVertical * sensitivity);
+
+        }
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle-click.");
+    }*/
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float rotateHorizontal = Input.GetAxis("Mouse X");
+            float rotateVertical = Input.GetAxis("Mouse Y");
+            transform.Rotate(-transform.up * rotateHorizontal * sensitivity);
+            transform.Rotate(transform.right * rotateVertical * sensitivity);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.KeypadPlus))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.KeypadMinus))
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+
+    }
+}
